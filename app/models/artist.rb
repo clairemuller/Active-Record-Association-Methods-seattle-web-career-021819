a9e32f22ff232a105ab623b334a04d02443f777b
clairemuller <clairermuller@gmail.com>
2019-03-04 16:57:14 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,25 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.count
+
+    # longer, more complicated way:
+    # genres = []
+    # self.songs.each do |song|
+    #   if !genres.include?(song.genre)
+    #     genres << song.genre
+    #   end
+    # end.count
   end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,23 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    Artist.find_or_create_by(name: "Drake")
+
+    # long, compliated way
+    # if Artist.all == []
+    #   drake = Artist.create(name: "Drake")
+    #   self.artist = drake
+    # elsif !Artist.all.include?(drake)
+    #   drake = Artist.create(name: "Drake")
+    #   self.artist = drake
+    # else
+    #   self.artist = drake
+    # end
   end
+
 end
